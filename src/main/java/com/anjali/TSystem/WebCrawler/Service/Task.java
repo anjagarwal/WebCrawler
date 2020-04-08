50bf61d9c2105a2a0624d5e1709c8879b1cb383d
anjagarwal <v-anjagarwal@expedia.com>
2020-04-09 02:19:24 +0530

    Created Web Crawler

--- src/main/java/com/anjali/TSystem/WebCrawler/Service/Task.java
+++ src/main/java/com/anjali/TSystem/WebCrawler/Service/Task.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
@@ -21,49 +22,53 @@ class Task extends RecursiveTask<WebCrawler> {
     public WebCrawler webCrawler;
     String url;
     int depth;
+    HashSet<String> links = new HashSet<>();
     Task(WebCrawler webCrawler, String url, int depth) {
         this.webCrawler = webCrawler;
         this.url = url;
         this.depth = depth;
-
     }
     @Override
     protected WebCrawler compute() {
         if (this.depth <=0) {
             return this.webCrawler;
         }
-        try {
-            DetailedLink detailedLink = new DetailedLink();
-            LOGGER.info("Connecting to given url through Jsoup");
-            Document document = Jsoup.connect(this.url).get();
-            Elements linksOnPage = document.select("a[href]");
-            Elements images = document.select("img");
-            Elements title = document.select("title");
-            detailedLink.setPage_title(title.text());
-            detailedLink.setImage_count(images.size());
-            detailedLink.setPage_link(this.url);
-            detailedLink.setDepth(this.depth);
-            detailedLink.setLinks_count(linksOnPage.size());
-            this.webCrawler.setTotal_images(images.size() + this.webCrawler.getTotal_images());
-            this.webCrawler.setTotal_links(linksOnPage.size() + this.webCrawler.getTotal_links());
-            this.webCrawler.getDetailedLinks().add(detailedLink);
-            this.depth--;
-            List<ForkJoinTask<WebCrawler>> resultList = new ArrayList<>();
-            for (Element link : linksOnPage) {
-                LOGGER.info("Creating a recusive Task");
-                Task recursiveTask = new Task(new WebCrawler(), link.attr("abs:href"), this.depth);
-                LOGGER.info("Forking the tasks");
-                resultList.add(recursiveTask.fork());
+        List<ForkJoinTask<WebCrawler>> resultList = new ArrayList<>();
+        if(!links.contains(this.url)) {
+            try {
+                links.add(this.url);
+                DetailedLink detailedLink = new DetailedLink();
+                LOGGER.info("Connecting to given url through Jsoup");
+                Document document = Jsoup.connect(this.url).get();
+                Elements linksOnPage = document.select("a[href]");
+                Elements images = document.select("img");
+                Elements title = document.select("title");
+                detailedLink.setPage_title(title.text());
+                detailedLink.setImage_count(images.size());
+                detailedLink.setPage_link(this.url);
+                detailedLink.setDepth(this.depth);
+                detailedLink.setLinks_count(linksOnPage.size());
+                this.webCrawler.setTotal_images(images.size() + this.webCrawler.getTotal_images());
+                this.webCrawler.setTotal_links(linksOnPage.size() + this.webCrawler.getTotal_links());
+                this.webCrawler.getDetailedLinks().add(detailedLink);
+                this.depth--;
+                for (Element link : linksOnPage) {
+                    LOGGER.info("Creating a recusive Task");
+                    Task recursiveTask = new Task(new WebCrawler(), link.attr("abs:href"), this.depth);
+                    LOGGER.info("Forking the tasks");
+                    resultList.add(recursiveTask.fork());
+                }
+
+            } catch (Exception ex) {
+                LOGGER.error("For url: " + url + " error message is: " + ex.getMessage());
             }
-            for (ForkJoinTask<WebCrawler> result : resultList) {
-                LOGGER.info("Get the result of recursive tasks");
-                WebCrawler wc = result.join();
-                this.webCrawler.getDetailedLinks().addAll(wc.getDetailedLinks());
-                this.webCrawler.setTotal_links(wc.getTotal_links() + this.webCrawler.getTotal_links());
-                this.webCrawler.setTotal_images(wc.getTotal_images() + this.webCrawler.getTotal_images());
-            }
-        } catch (Exception ex) {
-            LOGGER.error("For url: " + url + " error message is: " + ex.getMessage());
+        }
+        for (ForkJoinTask<WebCrawler> result : resultList) {
+            LOGGER.info("Get the result of recursive tasks");
+            WebCrawler wc = result.join();
+            this.webCrawler.getDetailedLinks().addAll(wc.getDetailedLinks());
+            this.webCrawler.setTotal_links(wc.getTotal_links() + this.webCrawler.getTotal_links());
+            this.webCrawler.setTotal_images(wc.getTotal_images() + this.webCrawler.getTotal_images());
         }
         return this.webCrawler;
     }
